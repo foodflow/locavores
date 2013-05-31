815a5a981cee72a664eaa3d8a5eb439a79013ef5
Michael Crumm <mike.crumm@groundctrl.com>
2013-05-31 00:14:29 -0700

    [ey] added before_bundle deploy script

---
+++ deploy/before_bundle.rb
@@ -0,0 +1,11 @@
+# prepare config files
+run "ln -s #{release_path}/web/app.php #{release_path}/web/index.php"
+
+# set timezone
+sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/cgi-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/cli-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/fpm-php5.4/ext-active/timezone.ini"
+
+# kick composer install
+run "curl -s https://getcomposer.org/installer | php -d allow_url_fopen=on"
+run "php -d allow_url_fopen=on composer.phar install"
