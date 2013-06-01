64bac1de6a3cfb1969f836c7d899f8bc83a6a309
Michael Crumm <mike.crumm@groundctrl.com>
2013-06-01 12:38:22 -0700

    source environment variables in deploy hook

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -6,6 +6,9 @@ sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/cgi-php5.4/ext-activ
 sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/cli-php5.4/ext-active/timezone.ini"
 sudo "echo 'date.timezone = America/Los_Angeles' > /etc/php/fpm-php5.4/ext-active/timezone.ini"
 
+# get environment vars
+run "source #{shared_path}/config/cli.env"
+
 # kick composer install
 run "curl -s https://getcomposer.org/installer | php -d allow_url_fopen=on"
 run "php -d allow_url_fopen=on composer.phar install"
